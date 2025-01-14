d86038564c90bf6512eeb8344f2f3c9578091a0d
arthuyang2003 <m18621118532@163.com>
2025-01-15 01:19:53 +0800

    添加脚本执行

--- real_world/pacs_s.sh
+++ real_world/pacs_s.sh
@@ -1,16 +1,17 @@
-declare -a seed=(5)
+declare -a seed=(4)
 declare -a s_dim=(4)
 declare -a lambda_vae=(5e-5)
 declare -a device=(0)
 
 for ((i=0;i<${#seed[@]};++i)); do
-WANDB_MODE=online CUDA_VISIBLE_DEVICES=${device[0]} python main2.py --root=../../da_datasets/pacs --batch-size=96 \
+WANDB_MODE=online CUDA_VISIBLE_DEVICES=${device[0]} python main2.py --root=../../da_datasets/pacs --batch-size=48 \
 -d PACS -s C,P,A -t S -a resnet18 \
---name=PACS_test \
+--name=PACS_test_To_S \
 --z_dim=64 --s_dim=${s_dim[0]} \
 --C_max=15 --beta=1 --lambda_vae=${lambda_vae[0]} --lambda_ent=0.1 \
--i=1200 \
+-i=2500 \
 --seed=${seed[i]} \
---epochs=40
+--epochs=40 \
+--finetune_epochs=20 \
 
 done
