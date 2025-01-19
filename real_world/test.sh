5d9bce36a4ec2b55ace24b57c0a0437e399f2378
arthuryang2003 <m18621118532@163.com>
2025-01-20 00:17:56 +0800

    test.sh修改

--- real_world/test.sh
+++ real_world/test.sh
@@ -4,16 +4,17 @@ declare -a lambda_vae=(5e-5)
 declare -a device=(0)
 
 for ((i=0;i<${#seed[@]};++i)); do
-WANDB_MODE=disabled CUDA_VISIBLE_DEVICES=${device[0]} python main2.py --root=../../da_datasets/pacs --batch-size=128 \
+WANDB_MODE=disabled CUDA_VISIBLE_DEVICES=${device[0]} python main2.py --root=../../da_datasets/pacs --batch-size=48 \
 -d PACS -s C,S,P -t A -a resnet18 \
 --name=PACS_test_To_A \
 --z_dim=64 --s_dim=${s_dim[0]} \
 --C_max=15 --beta=1 --lambda_vae=${lambda_vae[0]} --lambda_ent=0.1 \
--i=1 \
+-i=500 \
 --seed=${seed[i]} \
---finetune_epochs=1 \
---train_batch_size=32 \
+--finetune_epochs=2 \
+--train_batch_size=16 \
 --target_split_ratio=0.8 \
 --unstable_epochs=1 \
---stable_epochs=1
+--stable_epochs=1 \
+--phase=train
 done
