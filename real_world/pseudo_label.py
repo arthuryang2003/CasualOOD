b399622260b76143c5961e32e7887e9e7bd2db2a
arthuryang2003 <m18621118532@163.com>
2025-04-06 02:57:55 +0800

    changes

--- real_world/pseudo_label.py
+++ real_world/pseudo_label.py
@@ -21,11 +21,16 @@ def combined_inference(model, test_loader,num_classes):
 
             # 通过稳定模型提取特征并预测
             z_u,z_s,u_logits,s_logits,tilde_s_logits,combined_logits=model.encode(data)
-            stable_pred = F.softmax(u_logits, dim=1)  # 计算概率分布
-            stable_pred_hard = torch.argmax(stable_pred, dim=1)
 
-            # 计算未归一化的先验分布
-            PY_raw += stable_pred_hard.sum(dim=0)
+            # softmax 后的概率分布
+            stable_pred = F.softmax(u_logits, dim=1)
+
+            # 转为 one-hot 编码
+            stable_pred_hard = torch.argmax(stable_pred, dim=1)
+            stable_pred_onehot = F.one_hot(stable_pred_hard, num_classes=num_classes).float()
+
+            # 累加未归一化先验
+            PY_raw += stable_pred_onehot.sum(dim=0)
 
 
     # 计算归一化的 P_Y
