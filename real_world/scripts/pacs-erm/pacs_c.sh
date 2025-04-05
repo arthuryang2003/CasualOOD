a00f34092b73720b4f8245de74dd4df04b737a01
arthuryang2003 <m18621118532@163.com>
2025-04-05 17:44:32 +0800

    changes

---
+++ real_world/scripts/pacs-erm/pacs_c.sh
@@ -0,0 +1,18 @@
+declare -a seed=(8)
+declare -a device=(1)
+
+for ((i=0;i<${#seed[@]};++i)); do
+WANDB_MODE=disabled CUDA_VISIBLE_DEVICES=${device[0]} python ../../ERM.py --data_dir=../../data --dataset=PACS --batch-size=48 \
+-s A,S,P -t C -a resnet18 \
+--name=group3 \
+--z_dim=64 --hidden_dim=256 \
+-i=500 \
+--seed=${seed[i]} \
+--train_epochs=20 \
+--finetune_epochs=10 \
+--decouple_alpha=1.0 --decouple_beta=10.0 \
+--phase=train
+done
+
+
+
---
+++ real_world/scripts/pacs-irm/test.sh
@@ -0,0 +1,16 @@
+declare -a seed=(4)
+declare -a s_dim=(64)
+declare -a device=(0)
+
+for ((i=0;i<${#seed[@]};++i)); do
+WANDB_MODE=disabled CUDA_VISIBLE_DEVICES=${device[0]} python ../../IRM.py --data_dir=../../data --dataset=PACS --batch-size=48 \
+-s A,S,P -t C -a resnet18 \
+--name=group3 \
+--z_dim=64 --hidden_dim=256 \
+-i=100 \
+--seed=${seed[i]} \
+--train_epochs=1 \
+--finetune_epochs=1 \
+--decouple_alpha=1.0 --decouple_beta=10.0 \
+--phase=train
+done
