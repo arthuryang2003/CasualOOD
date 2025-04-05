ab938f611dab7e05570a3b07caace149a75c784f
arthuryang2003 <m18621118532@163.com>
2025-04-05 23:26:52 +0800

    changes

---
+++ real_world/scripts/CMINIST-erm/To1.sh
@@ -0,0 +1,15 @@
+declare -a seed=(4)
+declare -a device=(0)
+
+for ((i=0;i<${#seed[@]};++i)); do
+WANDB_MODE=disabled CUDA_VISIBLE_DEVICES=${device[0]} python ../../ERM.py --data_dir=../../data --dataset=ColoredMNIST --batch-size=256 \
+-s '+80%','-90%' -t'+90%' -a lenet \
+--name=test_To_3 \
+--z_dim=32 --hidden_dim=128 \
+-i=1000 \
+--seed=${seed[i]} \
+--train_epochs=20 \
+--finetune_epochs=10 \
+--decouple_alpha=1.0 --decouple_beta=10.0 \
+--phase=train
+done
---
+++ real_world/scripts/CMINIST-set4/test.sh
@@ -0,0 +1,15 @@
+declare -a seed=(4)
+declare -a device=(1)
+
+for ((i=0;i<${#seed[@]};++i)); do
+WANDB_MODE=disabled CUDA_VISIBLE_DEVICES=${device[0]} python ../../main.py --data_dir=../../data --dataset=ColoredMNIST --batch-size=48 \
+-s '+90%','+80%' -t'-90%' -a lenet \
+--name=group1 \
+--z_dim=32 --hidden_dim=128 \
+-i=100 \
+--seed=${seed[i]} \
+--train_epochs=1 \
+--finetune_epochs=1 \
+--decouple_alpha=1.0 --decouple_beta=10.0 \
+--phase=train
+done
